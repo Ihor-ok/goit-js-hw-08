b9a66a9504f0c3d76d195ef46a65d685b4040b5f
Ihor <kovaluk_igor@ukr.net>
2023-04-08 13:31:58 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -6,42 +6,111 @@ const refs = {
     textarea: document.querySelector('[name="message"]')
 };
 
-const formData = {email: '', message: ''};
+const STOREGE_KEY_INPUT = 'fedback-input-state';
+const STOREGE_KEY_TEXTAREA = 'fedback-textarea-state';
 
-const STOREGE_KEY = 'fedback-form-state';
+const formData = {
+
+};
 
 refs.form.addEventListener('submit', onFormSubmit);
-refs.form.addEventListener('input', throttle(onFormInput, 500));
+refs.input.addEventListener('input', throttle(onInput, 500));
+refs.textarea.addEventListener('input', throttle(onTextarea, 500));
 
-function onFormInput(evt) {
-
-    formData[evt.target.name] = evt.target.value;
-        
-    localStorage.setItem(STOREGE_KEY, JSON.stringify(formData));
-
-    }
+populateInput();
+populateTextarea();
 
 function onFormSubmit(evt) {
     evt.preventDefault();
     evt.currentTarget.reset();
+    
+
+    const savedFormInput = localStorage.getItem(STOREGE_KEY_INPUT);
+    const savedFormTextarea = localStorage.getItem(STOREGE_KEY_TEXTAREA);
+    
+    if (savedFormInput) {
+        formData.email = savedFormInput;
+    }
+
+      if (savedFormTextarea) {
+        formData.message = savedFormTextarea;
+    }
+
     console.log(formData);
-    localStorage.removeItem(STOREGE_KEY);
+
+    localStorage.removeItem(STOREGE_KEY_INPUT);
+    localStorage.removeItem(STOREGE_KEY_TEXTAREA);
 };
 
-populateForm();
+function onInput(evt) {
+    localStorage.setItem(STOREGE_KEY_INPUT, evt.target.value);
+    formData[evt.target.name] = evt.target.value;
 
-function populateForm() {
-    const savedForm = localStorage.getItem(STOREGE_KEY);
+};
 
-    if (savedForm) {
-     
-        const parsetLocalStorage = JSON.parse(savedForm);
+function onTextarea(evt) {
+    localStorage.setItem(STOREGE_KEY_TEXTAREA, evt.target.value);
+    formData[evt.target.name] = evt.target.value;
+};
 
-        const email = parsetLocalStorage.email;
-        const message = parsetLocalStorage.message;
+function populateInput() {
+    refs.input.value = localStorage.getItem(STOREGE_KEY_INPUT);
+};
 
-        refs.input.value = email;
-        refs.textarea.value = message;
+function populateTextarea() {
+    refs.textarea.value = localStorage.getItem(STOREGE_KEY_TEXTAREA);
+};
 
-    };
-}
+
+
+
+// Далі виконання завданя з збереженням в localStorage рядка у вигляді об'єкта з ключем'fedback-form-state'
+// така реалізаціє не дозволяє після перезавантаження сторінки змінювати в 
+// localStorage окремо значення Email або Message.
+
+
+// const formData = { email: '', message: '' };
+
+
+// const STOREGE_KEY = 'fedback-form-state';
+
+
+// refs.form.addEventListener('submit', onFormSubmit);
+// refs.form.addEventListener('input', throttle(onFormInput, 500));
+
+// populateForm();
+
+// function onFormInput(evt) {
+  
+//     formData[evt.target.name] = evt.target.value;
+//     localStorage.setItem(STOREGE_KEY, JSON.stringify(formData));
+// };
+
+
+
+// function onFormSubmit(evt) {
+//     evt.preventDefault();
+//     evt.currentTarget.reset();
+//     console.log(formData);
+//     localStorage.removeItem(STOREGE_KEY);
+// };
+
+
+
+// function populateForm() {
+//     const savedForm = localStorage.getItem(STOREGE_KEY);
+
+//     if (savedForm) {
+//         const parsetLocalStorage = JSON.parse(savedForm);
+
+//     console.log(parsetLocalStorage);
+
+//     const email = parsetLocalStorage.email;
+//     const message = parsetLocalStorage.message;
+    
+//     refs.input.value = email;
+//     refs.textarea.value = message;
+//     };
+    
+   
+// }
